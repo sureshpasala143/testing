25dc77e7ee7cb1052a70502aab8ed1efbef508e6
sureshpas <sureshpas@MTLP-3274.in.mouritech.net>
2022-11-16 15:13:30 +0530

    firstcommit

---
+++ Test/src/Add.java
@@ -0,0 +1,9 @@
+import com.google.errorprone.annotations.Var;
+import java.io.*;
+public class Add {
+	public static void main(String[] args){
+int a=10;
+int b=1;
+System.out.println("sum;" + a + b);
+	}
+}
